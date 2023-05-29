8d02a7bedaa6bcb502bdd38f62664ec4969a8843
Suzan <sys612603@gmail.com>
2023-05-29 06:10:16 +0300

    new

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LogoSearch from "../logoSearch/LogoSearch";
+import "./ProfileSide.css";
+import ProfileCard from "../profileCard/ProfileCard";
+import FollowersCard from "../followersCard/FollowersCard";
+const ProfileSide = () => {
+  return (
+    <div className="profileSide">
+      <LogoSearch />
+      <ProfileCard />
+      <FollowersCard />
+    </div>
+  );
+};
+
+export default ProfileSide;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import ProfileSide from '../../components/profileSide/ProfileSide'
+const Home = () => {
+  return (
+      <div className='Home'>
+         <ProfileSide/>
+          <div className="postSide">Posts</div>
+          <div className="rightSide">righSide</div>
+    </div>
+  )
+}
+
+export default Home
