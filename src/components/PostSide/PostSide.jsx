ed015d444486e02e2e6f99354ee7ef01402c2a16
Suzan <sys612603@gmail.com>
2023-06-04 02:59:29 +0300

    postsData

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./PostSide.css";
 import PostShare from "../PostShare/PostShare";
+import Posts from "../Posts/Posts";
 const PostSide = () => {
   return (
     <div className="PostSide">
       <PostShare />
+      <Posts />
     </div>
   );
 };
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Posts.css";
+const Posts = () => {
+  return <div>Posts</div>;
+};
+
+export default Posts;
