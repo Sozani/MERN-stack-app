e63c2b0079788bf05ffd2c112a29c8afd7d0b469
Suzan <sys612603@gmail.com>
2023-06-05 04:06:48 +0300

    InfoCard

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import "./InfoCard.css";
+import { UilPen } from "@iconscout/react-unicons";
 const InfoCard = () => {
-  return <div>InfoCard</div>;
+  return (
+    <div className="InfoCard">
+      <div className="infoHead">
+        <h4>Your Info</h4>
+        <div>
+          <UilPen width="2rem" height="1.2rem" />
+        </div>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Status</b>
+        </span>
+        <span> In Relationship</span>
+      </div>
+
+      <div className="info">
+        <span>
+          <b>Lives In</b>
+        </span>
+        <span> Multan</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Works At</b>
+        </span>
+        <span> Zeencodeskeeps inst</span>
+      </div>
+      <button className="button logout-button">Logout</button>
+    </div>
+  );
 };
 
 export default InfoCard;
