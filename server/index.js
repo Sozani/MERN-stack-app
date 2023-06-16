e273dca11eb6b249fdc505222b1a2a66b98c8670
Suzan <sys612603@gmail.com>
2023-06-17 01:46:57 +0300

    change

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+mongoose
+  .connect(
+    "mongodb+srv://ahemddoha:socialmedia123@cluster0.0jwhkdk.mongodb.net/Socialmedia?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() =>
+    app.listen(5000, () => {
+      console.log("listening....");
+    })
+  );
