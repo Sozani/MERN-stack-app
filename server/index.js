58f7188598a9c48e0910483b1f6ca4e671d3b46a
Suzan <sys612603@gmail.com>
2023-06-19 02:50:37 +0300

    change

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+dotenv.config();
 mongoose
-  .connect(
-    "mongodb+srv://ahemddoha:socialmedia123@cluster0.0jwhkdk.mongodb.net/Socialmedia?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(process.env.MANGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(5000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("listening....");
     })
   );
