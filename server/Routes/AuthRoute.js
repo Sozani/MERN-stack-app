f696d898c6de8714ff68751a1a79fee325bc428f
Suzan <sys612603@gmail.com>
2023-07-05 00:42:47 +0300

    post regsiterUser

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { registerUser } from "../Controllers/AuthController";
 const router = express.Router();
-
+router.post("./regsiter", registerUser);
 export default router;
