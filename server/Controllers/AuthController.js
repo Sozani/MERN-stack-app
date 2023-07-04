c7c05cd1a6ccbba45e13289416ad12db8a75ec91
Suzan <sys612603@gmail.com>
2023-07-05 00:14:22 +0300

    Add UserModel file

---
+++ server/Controllers/AuthController.js
@@ -0,0 +1,12 @@
+import UserModel from "../Models/userModel";
+//registering a new user
+export const registerUser = async (req, res) => {
+  const { username, lastname, password, firstname } = req.body;
+  const newUser = new UserModel({ username, lastname, password, firstname });
+  try {
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
