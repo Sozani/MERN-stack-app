b27f9bff4ea8f28037927700ff64f74d53f69ffd
Suzan <sys612603@gmail.com>
2023-07-06 08:16:09 +0300

    login user

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -5,11 +5,31 @@ export const registerUser = async (req, res) => {
   const { username, lastname, password, firstname } = req.body;
   const salt = await bcrypt.genSalt(10);
   const hashedPass = await bcrypt.hash(password, salt);
-  const newUser = new UserModel({ username, lastname, hashedPass, firstname });
+  const newUser = new UserModel({
+    username,
+    lastname,
+    password: hashedPass,
+    firstname,
+  });
   try {
     await newUser.save();
     res.status(200).json(newUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+//login user
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+      validity
+        ? res.status(200).json(user)
+        : res.status(400).json("Wrong Message");
+    } else {
+      res.status(404).json("User doesn't exists");
+    }
+  } catch (error) {}
 };
