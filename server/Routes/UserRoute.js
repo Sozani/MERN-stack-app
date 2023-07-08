34eec8ee5e17a5a6340d0eff3ccd209e04882ff9
Suzan <sys612603@gmail.com>
2023-07-08 22:05:58 +0300

    get id in UserRoute

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { getUser } from "../Controllers/UserController.js";
+// const router = express.Router();
+// router.get("/", (req, res) => {
+//   res.send("User Route");
+// });
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("User Route");
-});
+router.get("/:id", getUser);
 export default router;
