778984cd9f919588ec5aef8ef778e9f928f175b4
Suzan <sys612603@gmail.com>
2023-07-09 05:07:38 +0300

    update user

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,5 +1,5 @@
 import UserModel from "../Models/userModel.js";
-
+import bcrypt from "bcrypt";
 //get a User
 export const getUser = async (req, res) => {
   const id = req.params.id;
@@ -21,10 +21,18 @@ export const updateUser = async (req, res) => {
   const { currentUserId, currentUserAdminStatus, password } = req.body;
   if (id === currentUserId || currentUserAdminStatus) {
     try {
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
       const user = await UserModel.findByIdAndUpdate(id, req.body, {
         new: true,
       });
       res.status(200).json(user);
-    } catch (error) {}
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("Access Denied! you can only update your own profile");
   }
 };
