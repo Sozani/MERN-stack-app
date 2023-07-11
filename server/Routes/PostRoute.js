ac4131e616487f2ee076347260dfe2fcd68e77a3
Suzan <sys612603@gmail.com>
2023-07-11 04:38:50 +0300

    Create post

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,3 +1,5 @@
 import express from "express";
+import { createPost } from "../Controllers/PostController.js";
 const router = express.Router();
+router.get("/", createPost);
 export default router;
