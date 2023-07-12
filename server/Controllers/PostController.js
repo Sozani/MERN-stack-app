fcd9898eeee567f9600bf3e8dc2b2e1ae422f435
Suzan <sys612603@gmail.com>
2023-07-13 01:59:39 +0300

    Delete a post

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -37,3 +37,20 @@ export const updatePost = async (req, res) => {
     res.status(500).json(error);
   }
 };
+//Delete post
+
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await PostModel.findById(id);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json("Post deleted successfully");
+    } else {
+      res.status(403).json("Action forbidden");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -3,9 +3,11 @@ import {
   createPost,
   getPost,
   updatePost,
+  deletePost,
 } from "../Controllers/PostController.js";
 const router = express.Router();
 router.post("/", createPost);
 router.get("/:id", getPost);
 router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 export default router;
