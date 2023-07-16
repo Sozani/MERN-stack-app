27b1c0faed94b3cb2ebbd4c5444ffc7b8ded7e84
Suzan <sys612603@gmail.com>
2023-07-16 07:31:52 +0300

    authAction

--- src/actions/AuthAction.js
+++ src/actions/AuthAction.js
@@ -1,3 +1,10 @@
-export const logIn = (dataForm) => async (dispatch) => {
-  const { data } = await AuthApi.logIn(formData);
+import * as AuthApi from "../api/AuthRequest.jsx";
+export const logIn = (formData) => async (dispatch) => {
+  dispatch({ type: "AUTH-START" });
+  try {
+    const { data } = await AuthApi.logIn(formData);
+    dispatch({ type: "AUTH-SUCCESS", data: data });
+  } catch (error) {
+    console.log(error);
+  }
 };
