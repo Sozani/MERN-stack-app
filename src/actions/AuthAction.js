da3ac2e005c6e50e4ca7018d30fb8f9050200f80
Suzan <sys612603@gmail.com>
2023-07-16 08:00:33 +0300

    signUp in AuthAction

--- src/actions/AuthAction.js
+++ src/actions/AuthAction.js
@@ -9,3 +9,13 @@ export const logIn = (formData) => async (dispatch) => {
     console.log(error);
   }
 };
+export const signUp = (formData) => async (dispatch) => {
+  dispatch({ type: "AUTH-START" });
+  try {
+    const { data } = await AuthApi.signUp(formData);
+    dispatch({ type: "AUTH-SUCCESS", data: data });
+  } catch (error) {
+    dispatch({ type: "AUTH-FAIL" });
+    console.log(error);
+  }
+};
