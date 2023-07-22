29656f04f49d0289bc139eb8f0fc0770aa3f9e2e
Suzan <sys612603@gmail.com>
2023-07-22 04:41:09 +0300

    upload a post with data and image

--- src/actions/UploadAction.js
+++ src/actions/UploadAction.js
@@ -10,7 +10,7 @@ export const uploadPost = (data) => async (dispatch) => {
   dispatch({ type: "UPLOAD_START" });
   try {
     const newPost = await UploadApi.uploadPost(data);
-    dispatch({ type: "UPLOAD_SUCCESS" });
+    dispatch({ type: "UPLOAD_SUCCESS", data: newPost.data });
   } catch (error) {
     console.log(error);
     dispatch({ type: "UPLOAD_FAIL" });
