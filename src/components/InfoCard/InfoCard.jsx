e108ee76a9ecf198ae68932b6297a91ffb05f1e2
Suzan <sys612603@gmail.com>
2023-07-25 01:20:25 +0300

    Integrate Info card with server

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import "./InfoCard.css";
 import { UilPen } from "@iconscout/react-unicons";
 import ProfileModal from "../ProfileModel.jsx/ProfileModel";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import * as userApi from "../../api/UserRequests.js";
 const InfoCard = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const profileUserId = params.id;
+  const [profileUser, setProfileUser] = useState({});
+  const { user } = useSelector((state) => state.authReducer.authData);
   const [modalOpened, setModalOpened] = useState(false);
+  useEffect(() => {
+    const fetchProfileUser = async () => {
+      if (profileUserId === user._id) {
+        setProfileUser(user);
+      } else {
+        const profileUser = await userApi.getUser(profileUserId);
+        setProfileUser(profileUser);
+      }
+    };
+    fetchProfileUser();
+  }, [user]);
   return (
     <div className="InfoCard">
       <div className="infoHead">
-        <h4>Your Infomation</h4>
+        <h4>Profile Info</h4>
         <div>
           <UilPen
             width="2rem"
