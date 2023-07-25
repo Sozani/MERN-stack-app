dfbeba59a66befc8a8d798e14ad6b26f442c6b01
Suzan <sys612603@gmail.com>
2023-07-25 08:29:16 +0300

    Change style to follow button

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -32,7 +32,7 @@ export const getUser = async (req, res) => {
 export const updateUser = async (req, res) => {
   const id = req.params.id;
 
-  const { _id, currentUserAdminStatus, password } = req.body;
+  const { _id, currentUserAdmin, password } = req.body;
   if (id === _id) {
     try {
       if (password) {
@@ -75,15 +75,16 @@ export const deleteUser = async (req, res) => {
 //Follow a user
 export const followUser = async (req, res) => {
   const id = req.params.id;
-  const { currentUserId } = req.body;
-  if (currentUserId === id) {
+  const { _id } = req.body;
+  console.log(id, _id);
+  if (_id === id) {
     res.status(403).json("Action forbiden");
   } else {
     try {
       const followUser = await UserModel.findById(id);
-      const followingUser = await UserModel.findById(currentUserId);
-      if (!followUser.followers.includes(currentUserId)) {
-        await followUser.updateOne({ $push: { follower: currentUserId } });
+      const followingUser = await UserModel.findById(_id);
+      if (!followUser.followers.includes(_id)) {
+        await followUser.updateOne({ $push: { followers: _id } });
         await followingUser.updateOne({ $push: { following: id } });
         res.status(200).json("User followed!");
       } else {
@@ -97,19 +98,19 @@ export const followUser = async (req, res) => {
 //Unfollow a user
 export const unFollowUser = async (req, res) => {
   const id = req.params.id;
-  const { currentUserId } = req.body;
-  if (currentUserId === id) {
-    res.status(403).json("Action forbiden");
+  const { _id } = req.body;
+  if (_id === id) {
+    res.status(403).json("Action forbidden");
   } else {
     try {
-      const followUser = await UserModel.findById(id);
-      const followingUser = await UserModel.findById(currentUserId);
-      if (followUser.followers.includes(currentUserId)) {
-        await followUser.updateOne({ $pull: { follower: currentUserId } });
-        await followingUser.updateOne({ $pull: { following: id } });
-        res.status(200).json("User unfollowed!");
+      const unFollowUser = await UserModel.findById(id);
+      const unFollowingUser = await UserModel.findById(_id);
+      if (unFollowUser.followers.includes(_id)) {
+        await unFollowUser.updateOne({ $pull: { followers: _id } });
+        await unFollowingUser.updateOne({ $pull: { following: id } });
+        res.status(200).json("Unfollowed Successfully!");
       } else {
-        res.status(403).json("User is not followed by you");
+        res.status(403).json("You are not following this User");
       }
     } catch (error) {
       res.status(500).json(error);
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unfollowUser } from "../../api/UserRequests";
 
 function User(person) {
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const dispatch = useDispatch();
+  const [following, setFollowing] = useState(
+    person.following.includes(user._id)
+  );
+  const handleFollow = () => {
+    following
+      ? dispatch(unfollowUser(person._id, user))
+      : dispatch(followUser(person._id, user));
+    setFollowing((prev) => !prev);
+  };
   return (
     <div className="follower">
       <div>
-        <img src={person.img} alt="" className="followerImage" />
+        <img
+          src={
+            person.coverPicture
+              ? serverPublic + person.profilePicture
+              : serverPublic + "defaultProfile.png"
+          }
+          alt=""
+          className="followerImage"
+        />
         <div className="name">
-          <span>{person.name}</span>
+          <span>{person.firstname}</span>
           <span>{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button">Follow</button>
+      <button
+        className={
+          following ? "button fc-button UnfollowButton" : "button fc-button"
+        }
+        onClick={handleFollow}
+      >
+        {following ? "Unfollow" : "follow"}
+      </button>
     </div>
   );
 }
