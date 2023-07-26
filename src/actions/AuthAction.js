75d39209450872d52f832ac19942212bb8dd4b17
Suzan <sys612603@gmail.com>
2023-07-27 02:07:26 +0300

    Navigate to home page in Auth action

--- src/actions/AuthAction.js
+++ src/actions/AuthAction.js
@@ -1,20 +1,22 @@
 import { useAsyncError } from "react-router-dom";
 import * as AuthApi from "../api/AuthRequest.jsx";
-export const logIn = (formData) => async (dispatch) => {
+export const logIn = (formData, navigate) => async (dispatch) => {
   dispatch({ type: "AUTH-START" });
   try {
     const { data } = await AuthApi.logIn(formData);
     dispatch({ type: "AUTH-SUCCESS", data: data });
+    navigate("../home", { replace: true });
   } catch (error) {
     dispatch({ type: "AUTH-FAIL" });
     console.log(error);
   }
 };
-export const signUp = (formData) => async (dispatch) => {
+export const signUp = (formData, navigate) => async (dispatch) => {
   dispatch({ type: "AUTH-START" });
   try {
     const { data } = await AuthApi.signUp(formData);
     dispatch({ type: "AUTH-SUCCESS", data: data });
+    navigate("../home", { replace: true });
   } catch (error) {
     dispatch({ type: "AUTH-FAIL" });
     console.log(error);
